c3b5d45f5fde2491c9da03022bdde06a2eb5898a
CarlGustav <singhrajvardhan2003@gmail.com>
2025-04-02 10:57:36 +0530

    Fibonacci in rust

--- kirat_rust_marathon/rust-1/src/main.rs
+++ kirat_rust_marathon/rust-1/src/main.rs
@@ -1,12 +1,6 @@
+mod utils;
 fn main() {
-    println!("{}", is_even(20));
+    println!("{}", utils::is_even(20));
+    println!("{}", utils::fibonacci(20));
 }
 
-//i in i32 stands for signed which means the input can be +ve or -ve as well, 32 stands for 32 bits
-
-fn is_even(num:i32)->bool{
-    if num%2==0{
-        return true;
-    }
-    return false;
-}
---
+++ kirat_rust_marathon/rust-1/src/utils.rs
@@ -0,0 +1,26 @@
+//i in i32 stands for signed which means the input can be +ve or -ve as well, 32 stands for 32 bits
+
+pub fn is_even(num: i32) -> bool {
+    if num % 2 == 0 {
+        return true;
+    }
+    return false;
+}
+
+pub fn fibonacci(num: i32) -> i32 {
+    let mut first = 0;
+    let mut second = 1;
+
+    if num == 0 {
+        return first;
+    }
+    if num == 1 {
+        return second;
+    }
+    for _i in 1..num - 2 {
+        let temp = second;
+        second = second + first;
+        first = temp;
+    }
+    return second;
+}
